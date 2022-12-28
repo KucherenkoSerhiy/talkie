641e4fe634cf5fde28dd016c4296584a0968836b
skucherenko <kucherenkoserhiy@gmail.com>
2022-12-28 01:57:28 +0100

    fixed file path

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ file_name = youtube_video.title + ".mp4"
 file_name = re.sub('[^A-Za-z0-9 .]+', '', file_name)
 
 # Download the audio stream and save it to the file
-# WARNING: it saves the file inside the subdirectory with the same name
-audio_stream.download(file_name)
+audio_stream.download(filename=file_name)
 
 # Load the input audio file
 input_audio = ffmpeg.input(file_name)
@@ -28,20 +27,15 @@ input_audio = ffmpeg.input(file_name)
 # Use the trim filter to specify the start and end times for the trimmed audio
 trimmed_audio = input_audio.filter_('trim', start='00:00:10', end='00:00:20')
 
-
-# Get the absolute path of the input file
-# Set the path according to how it was saved (see the previous WARNING comment line)
-file_path = os.path.abspath(f"{file_name}\\{file_name}")
-
 # Use ffmpeg.output to specify the output file and run the conversion
-ffmpeg.output(trimmed_audio, file_path, overwrite_output=True).run()
+ffmpeg.output(trimmed_audio, file_name, overwrite_output=True).run()
 
 print(f"analyzing {youtube_video.title}")
 print("------------------------------------------------")
 
 t1 = datetime.datetime.now()
 print(f"started at {t1}")
-result = model.transcribe(youtube_video)
+result = model.transcribe(file_name)
 t2 = datetime.datetime.now()
 print(f"ended at {t2} (elapsed {t2 - t1})")
 
